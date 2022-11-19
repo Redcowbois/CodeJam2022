bd44dc355032b909056de9183fc8855d3b4b5037
Jiaqi Yang <jiayang@outlook.fr>
2022-11-18 22:50:19 -0500

    game loop

--- index.py
+++ index.py
@@ -1,5 +1,19 @@
 #Main File
 
 import pygame
+from sys import exit 
+pygame.init()
 
+window = pygame.display.set_mode((100, 562))
+pygame.display.set_caption("CodeJam")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    clock.tick(60)
 
