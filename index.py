ecc13371df7d942f865bf48c867aca620a80750c
Jiaqi Yang <jiayang@outlook.fr>
2022-11-19 12:05:34 -0500

    added tiles and assets

--- index.py
+++ index.py
@@ -1,14 +1,33 @@
 #Main File
 
+###Setup###
+
 import pygame
 from sys import exit 
 pygame.init()
 
+from tiles import *
+
 window = pygame.display.set_mode((1024, 576))
 pygame.display.set_caption("CodeJam")
 clock = pygame.time.Clock()
+###
 
+
+###Textures###
+
+background = pygame.image.load("./assets/space.jpg").convert_alpha()
+###
+
+
+###Game Loop###
 while True:
+    window.blit(background, (0,0))
+
+    generated_tiles = draw_tiles(window, display_map)
+    for tile in generated_tiles:
+        window.blit(tile.image, tile.rect)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -16,4 +35,4 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
-
+###
--- tiles.py
+++ tiles.py
@@ -1,2 +1,30 @@
 import pygame
 
+class FloorTile(pygame.sprite.Sprite):              #Class for floor tiles
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.image.load("./assets/floor.png").convert_alpha()
+        self.rect = self.image.get_rect(bottomleft = (pos_x, pos_y))
+        self.id = "10"                                #Id here is 10 so we can easily have sub id #11, #12...
+
+
+display_map = [[],[],[],[],[],
+                [],[],[10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10],[]]
+
+def draw_tiles(window, display_map):
+    """
+    Draws the tiles, takes the window its gonna draw it to and the 
+    display map as inputs. Returns nothing.
+    """
+    generated_tiles = []
+
+    for row in range(len(display_map)):
+        if len(display_map[row]) == 0: #Skips an iteration if there is nothing in the row
+            continue
+
+        for col in range(len(display_map[row])):
+            if display_map[row][col] == 10:
+                generated_tiles.append(FloorTile(col*64, row*64))
+    
+    return generated_tiles
+
